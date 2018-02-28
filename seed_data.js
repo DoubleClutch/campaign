24168580b9ee9b3d43ee17798b26d1d7fbbc3e90
Chazie9 <charlesharring3@gmail.com>
2018-02-28 11:04:37 -0800

    added seeding files and modified schema

---
+++ seed_data.js
@@ -0,0 +1,54 @@
+module.exports.campaignInfo = [
+    {
+        id: 1,
+        title: 'Ledr: the tool roll for everyone!', 
+        owner: 'Bobby & Calvin', 
+        location: 'Burbank, CA', 
+        videoURL: 'https://ksr-video.imgix.net/projects/3260803/video-857213-h264_high.mp4', 
+        tagline:'From pens & cables to wrenches & screwdrivers, bring order to your every day tools!', 
+        previousCampaigns: 5,
+        category: 'A tool roll is one of the most versatile packing accessories for organizing a large amount of items in a very small space. It\'s super compact when rolled up for travel, yet perfectly organized when opened for use. Most are designed with the kitchen or workshop in mind, but Ledr tool rolls are designed for everyone, to use anywhere to carry anything!',
+        about: [
+            {type: 'heading', value: 'this is a sample about heading'}, 
+            {type: 'block', value: 'This is a block of text'}, 
+            {type: 'img', value: 'an image url'}]
+    },
+    {
+        id: 2,
+        title: 'Another title for a project', 
+        owner: 'Dave & Calvin Laituri', 
+        location: 'Saxonville, Framingham, MA', 
+        videoURL: 'https://ksr-video.imgix.net/projects/3260803/video-857213-h264_high.mp4', 
+        tagline:'From pens & cables to wrenches & screwdrivers, bring order to your every day tools!', 
+        previousCampaigns: 3,
+        category: 'A tool roll is one of the most versatile packing accessories for organizing a large amount of items in a very small space. It\'s super compact when rolled up for travel, yet perfectly organized when opened for use. Most are designed with the kitchen or workshop in mind, but Ledr tool rolls are designed for everyone, to use anywhere to carry anything!',
+        about: [
+            {type: 'heading', value: 'this is a sample about heading'}, 
+            {type: 'block', value: 'This is a block of text'}, 
+            {type: 'img', value: 'an image url'},
+            {type: 'heading', value: 'this is a sample about heading'}, 
+            {type: 'img', value: 'an image url'},
+            {type: 'img', value: 'an image url'},
+            {type: 'block', value: 'This is a block of text'}, 
+        ]
+    },
+    {
+        id: 3,
+        title: 'KOMA: D-type Battery Powered Vehicle Jump Starter', 
+        owner: 'Gagyo Tech', 
+        location: 'Saxonville, Framingham, MA', 
+        videoURL: 'https://ksr-video.imgix.net/projects/3260803/video-857213-h264_high.mp4', 
+        tagline:'The only vehicle jump starter equipped with a Joule Thief voltage booster. Start up any car, truck, and even small ships under 25 tons.', 
+        previousCampaigns: 3,
+        category: 'A tool roll is one of the most versatile packing accessories for organizing a large amount of items in a very small space. It\'s super compact when rolled up for travel, yet perfectly organized when opened for use. Most are designed with the kitchen or workshop in mind, but Ledr tool rolls are designed for everyone, to use anywhere to carry anything!',
+        about: [
+            {type: 'heading', value: 'this is a sample about heading'}, 
+            {type: 'block', value: 'This is a block of text'}, 
+            {type: 'img', value: 'an image url'},
+            {type: 'heading', value: 'this is a sample about heading'}, 
+            {type: 'img', value: 'an image url'},
+            {type: 'img', value: 'an image url'},
+            {type: 'block', value: 'This is a block of text'}, 
+        ]
+    },
+]
